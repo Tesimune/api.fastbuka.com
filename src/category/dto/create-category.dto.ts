799f1e84883c193ba3b6ca8b32ace09e555d2d50
Younglad-Paul <borisadedayopaul@gmail.com>
2024-10-16 19:03:35 +0100

    created the category mutation

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,1 +1,12 @@
-export class CreateCategoryDto {}
+import { IsString } from 'class-validator';
+
+export class CreateCategoryDto {
+  @IsString()
+  vendor_uuid: string;
+
+  @IsString()
+  name: string;
+
+  @IsString()
+  image: string;
+}
