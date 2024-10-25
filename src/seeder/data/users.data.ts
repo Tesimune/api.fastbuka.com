6627d7423431015109fc4043438f65ae21c24ab4
Tesimune <tesimune@gmail.com>
2024-10-25 12:09:42 +0100

    Updated migration

--- src/seeder/data/users.data.ts
+++ src/seeder/data/users.data.ts
@@ -9,15 +9,66 @@ export const users = [
   {
     name: 'John Doe',
     email: 'john@example.com',
-    password: 'password123',
+    password: '@password1',
     contact: '1234567890',
     role: 'vendor',
   },
   {
     name: 'Jane Doe',
     email: 'jane@example.com',
-    password: 'password123',
+    password: '@password1',
     contact: '0987654321',
     role: 'user',
   },
+  {
+    name: 'Anthony Marshall',
+    email: 'anthony@example.com',
+    password: '@password1',
+    contact: '0987654321',
+    role: 'user',
+  },
+  {
+    name: 'John Yoho',
+    email: 'john@example.com',
+    password: '@password1',
+    contact: '0987654321',
+    role: 'user',
+  },
+
+  {
+    name: 'Borisade Paul',
+    email: 'borisadedayopaul@gmail.com',
+    password: '12345678',
+    contact: '1234509876',
+    role: 'admin',
+  },
+  {
+    name: 'James Chowdhury',
+    email: 'james@example.com',
+    password: '@password1',
+    contact: '1234567890',
+    role: 'vendor',
+  },
+  {
+    name: 'Hugh Myers',
+    email: 'hugh@example.com',
+    password: '@password1',
+    contact: '0987654321',
+    role: 'user',
+  },
+  {
+    name: 'Russel Felton',
+    email: 'russel@example.com',
+    password: '@password1',
+    contact: '0987654321',
+    role: 'user',
+  },
+  {
+    name: 'Paul Blow',
+    email: 'paul@example.com',
+    password: '@password1',
+    contact: '0987654321',
+    role: 'user',
+  },
+
 ];
